a44e25b173de71e7351006e71282713b12e749e7
Hong5489 <hongwei5489@gmail.com>
2020-08-09 15:58:01 +0800

    update

--- CTFd/plugins/flags/__init__.py
+++ CTFd/plugins/flags/__init__.py
@@ -89,7 +89,7 @@ class DynamicFlag(BaseFlag):
 
         if share:
             from CTFd.utils.events import socketio
-            socketio.emit('notification', get_current_team().name, broadcast=True)
+            socketio.emit('notification', {"data": get_current_team().name}, broadcast=True)
         return result == 0,share
 
 
